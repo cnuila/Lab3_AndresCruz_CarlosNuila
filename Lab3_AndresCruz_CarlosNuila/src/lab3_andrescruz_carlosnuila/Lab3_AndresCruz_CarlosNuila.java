dd9cb4164dd6f9213b634ed50b6486b77c3b716c
Carlos Nuila <cnuila14@icloud.com>
2018-10-26 17:31:29 -0600

    Actualizacion

--- Lab3_AndresCruz_CarlosNuila/src/lab3_andrescruz_carlosnuila/Lab3_AndresCruz_CarlosNuila.java
+++ Lab3_AndresCruz_CarlosNuila/src/lab3_andrescruz_carlosnuila/Lab3_AndresCruz_CarlosNuila.java
@@ -6,11 +6,8 @@
 package lab3_andrescruz_carlosnuila;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
-<<<<<<< HEAD
-
-=======
->>>>>>> 7b39023ec5d17814da582b1dd996cdbfa63d0ff9
 
 /**
  *
@@ -25,14 +22,29 @@ public class Lab3_AndresCruz_CarlosNuila {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int opcionMenu = 0;
-<<<<<<< HEAD
-
         ArrayList<Ejercito> listaEjercito = new ArrayList();
         ArrayList<Arma> listaArma = new ArrayList();
-=======
-        ArrayList <Ejercito> listaEjercito = new ArrayList();
-        ArrayList <Arma> listaArma = new ArrayList();
->>>>>>> 7b39023ec5d17814da582b1dd996cdbfa63d0ff9
+        listaEjercito.add(new Ejercito("Cyrodill", "Tamriel", 40000000));
+        Random r = new Random();
+        listaEjercito.add(new Ejercito("Skyrim", "Tamriel", 20000000));
+        String[] nombres = {"Oscar", "Joel", "Carlos", "Andres", "Diego", "Daniel", "Cristopher", "Ricardo", "Eduardo", "Manuel"};
+        String[] lugares = {"Skyrim", "Cyrodill", "Valenwood", "Elsweyr", "Black Marsh", "Morrowind"};
+        for (int i = 0; i < listaEjercito.size(); i++) {
+            for (int j = 0; j < 5; j++) {
+                int nom_alea = r.nextInt(10);
+                int lug_alea = r.nextInt(6);
+                listaEjercito.get(i).getListaSoldado().add(new Soldado(nombres[nom_alea], lugares[lug_alea], (20 + r.nextInt(40)), 100));
+                if (j == 0) {
+                    listaEjercito.get(i).getListaSoldado().add(new General((1 + r.nextInt(10)), nombres[nom_alea], lugares[lug_alea], (20 + r.nextInt(40)), 100));
+                } else {
+                    if (j >= 1 && j <= 2) {
+                        listaEjercito.get(i).getListaSoldado().add(new Caballero(nombres[nom_alea], lugares[lug_alea], (20 + r.nextInt(40)), 100));
+                    } else {
+                        listaEjercito.get(i).getListaSoldado().add(new Ejecutor(nombres[nom_alea], lugares[lug_alea], (20 + r.nextInt(40)), 100));
+                    }
+                }
+            }
+        }
         while (opcionMenu != 5) {
             System.out.println("1)Crear Ejercitos\n"
                     + "2)Crear Soldados\n"
@@ -93,7 +105,6 @@ public class Lab3_AndresCruz_CarlosNuila {
                         } else {
                             System.out.println("El nodo seleccionado no existe");
                         }
-<<<<<<< HEAD
                     }
 
                     if (opcionEjercito == 3) {
@@ -155,20 +166,20 @@ public class Lab3_AndresCruz_CarlosNuila {
                                     System.out.print("Ingrese el tipo de soldado");
                                     tipoSoldado = sc.nextInt();
                                 }
-                                if (tipoSoldado == 1){
-                                    if (contGenerales == 0){
+                                if (tipoSoldado == 1) {
+                                    if (contGenerales == 0) {
                                         System.out.print("Ingrese los años al mando: ");
                                         int añoAlmando = sc.nextInt();
                                         listaEjercito.get(posicionEjercito).getListaSoldado().add(new General(añoAlmando, nombre, lugar, edad, salud));
                                     }
                                 }
-                                if (tipoSoldado == 2){
-                                    if (contCaballeros <= 3){
+                                if (tipoSoldado == 2) {
+                                    if (contCaballeros <= 3) {
                                         listaEjercito.get(posicionEjercito).getListaSoldado().add(new Caballero(nombre, lugar, edad, salud));
                                     }
                                 }
-                                if (tipoSoldado == 3){
-                                    if (contEjecutor == 0){
+                                if (tipoSoldado == 3) {
+                                    if (contEjecutor == 0) {
                                         listaEjercito.get(posicionEjercito).getListaSoldado().add(new Ejecutor(nombre, lugar, edad, salud));
                                     }
                                 }
@@ -176,20 +187,77 @@ public class Lab3_AndresCruz_CarlosNuila {
                                 System.out.print("Ya tiene el maximo numero de soldados");
                             }
                         }
-                        if (opcionSoldado == 2){
+                        if (opcionSoldado == 2) {
                             System.out.print("Ingrese la posicion del soldado: ");
                             int posicionSoldado = sc.nextInt();
-                            if (posicionSoldado < listaEjercito.get(posicionEjercito).getListaSoldado().size()){
-                                
+                            if (posicionSoldado < listaEjercito.get(posicionEjercito).getListaSoldado().size()) {
+                                System.out.println("1)Nombre\n"
+                                        + "2)Lugar\n"
+                                        + "3)Edad\n"
+                                        + "4)Salud\n"
+                                        + "5)Anios al mando\n"
+                                        + "6)Atras");
+                                int opcionModificar = sc.nextInt();
+                                if (opcionModificar == 1) {
+                                    System.out.print("Ingrese el nombre del Soldado: ");
+                                    String nombre = sc.next();
+                                    listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado).setNombre(nombre);
+                                    System.out.println("Modificacion Exitosa");
+                                }
+                                if (opcionModificar == 2) {
+                                    System.out.print("Ingrese el lugar de nacimiento del Soldado: ");
+                                    String lugar = sc.next();
+                                    listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado).setLugar(lugar);
+                                    System.out.println("Modificacion Exitosa");
+                                }
+                                if (opcionModificar == 3) {
+                                    System.out.print("Ingrese la edad del soldado: ");
+                                    int edad = sc.nextInt();
+                                    listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado).setEdad(edad);
+                                    System.out.println("Modificacion Exitosa");
+                                }
+                                if (opcionModificar == 4) {
+                                    System.out.print("Ingrese la salud del soldado: ");
+                                    int salud = sc.nextInt();
+                                    listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado).setSalud(salud);
+                                    System.out.println("Modificacion Exitosa");
+                                }
+                                if (opcionModificar == 5) {
+                                    if (listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado) instanceof General) {
+                                        System.out.print("Ingrese los años al mando: ");
+                                        int añoAlmando = sc.nextInt();
+                                        ((General) listaEjercito.get(posicionEjercito).getListaSoldado().get(posicionSoldado)).setAñoAlmando(añoAlmando);
+                                    } else {
+                                        System.out.println("No puede realizar esta opcion");
+                                    }
+                                }
+
                             } else {
                                 System.out.println("El nodo seleccionado no esta disponible");
                             }
                         }
+                        if (opcionSoldado == 3) {
+                            System.out.print("Ingrese la posicion del soldado: ");
+                            int posicionSoldado = sc.nextInt();
+                            if (posicionSoldado < listaEjercito.get(posicionEjercito).getListaSoldado().size()) {
+                                listaEjercito.get(posicionEjercito).getListaSoldado().remove(posicionSoldado);
+                            } else {
+                                System.out.println("No existe el nodo");
+                            }
+                        }
+                        if (opcionSoldado == 4) {
+                            for (int i = 0; i < listaEjercito.get(posicionEjercito).getListaSoldado().size(); i++) {
+                                System.out.print(listaEjercito.get(posicionEjercito).getListaSoldado().get(i));
+                            }
+                            System.out.println();
+                        }
                     }
-=======
-                    }                    
->>>>>>> 7b39023ec5d17814da582b1dd996cdbfa63d0ff9
+                } else {
+                    System.out.println("No existe el ejercito");
                 }
+            }
+            if (opcionMenu == 3){
+                System.out.println("");
             }
         }
     }
@@ -198,9 +266,15 @@ public class Lab3_AndresCruz_CarlosNuila {
         for (int i = 0; i < 10; i++) {
             System.out.print("----");
         }
+        System.out.println("");
     }
-<<<<<<< HEAD
-    
-=======
->>>>>>> 7b39023ec5d17814da582b1dd996cdbfa63d0ff9
+
+    public static void opc4(ArrayList<Ejercito> listaEjercito) {
+        for (int i = 0; i < listaEjercito.size(); i++) {
+            System.out.println(listaEjercito.get(i));
+            espacio();
+            System.out.println("");
+        }
+
+    }
 }
